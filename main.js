01e883dc09179c2163030b2720a88b33a68f8292
Rudziks <Rozvilla12@o2.pl>
2021-03-26 15:35:13 +0100

    tak

--- main.js
+++ main.js
@@ -1,8 +1,9 @@
 //do każdego z osobna dajemy consol loga aby czytało daną czynność, a nie cały kod z js.
 function wyslij() { 
-const imie = document.getElementById("name").value
+const imie = document.getElementById("imie").value
+    console.log(imie)
 const nazwisko = document.getElementById("sname").value
-    console.log(imie + ' ' + nazwisko)
+    console.log(nazwisko)
 const mail = document.getElementById("mail").value
     console.log(mail)
 const tel = document.getElementById("tel").value
@@ -22,4 +23,35 @@ const tel = document.getElementById("tel").value
     console.log(tel)
 const info = document.getElementById("info").value
     console.log(info)
+}
+
+//nie krotsze od 3 znakow imie
+function walidacja() {
+const imie = document.getElementById("imie").value
+if (imie.length < 3) {
+    alert("ZA KRÓTKIE IMIE")
+}
+const nazwisko = document.getElementById("sname").value
+if(nazwisko.length < 5){
+    alert("ZA KRÓTKIE NAZWISKO")
+}
+}
+
+function email(val){
+    let xd = val.split('')
+    console.log(xd)
+    const isEmail = xd.some(item => item === "@")
+    console.log(isEmail)
+}
+
+function telefon() {
+    const tel = document.getElementById("tel").value
+    const isNum = Number()
+}
+
+
+
+function pizza(pizza){
+    const wybranaPizza = pizza
+    console.log(wybranaPizza)
 }
