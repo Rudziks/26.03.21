fb2b1e45b085ee9ddb5c310292880bcb2c2fb2fb
Rudziks <Rozvilla12@o2.pl>
2021-03-26 10:43:48 +0100

    onchange na inputach

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+//do każdego z osobna dajemy consol loga aby czytało daną czynność, a nie cały kod z js.
 function wyslij() { 
 const imie = document.getElementById("name").value
 const nazwisko = document.getElementById("sname").value
@@ -10,17 +11,15 @@ const info = document.getElementById("info").value
     console.log(info)
 }
 
-
-function pobieranie() {
-    console.log("-----------------")
-    const imie = document.getElementById("imie").value
-    const nazwisko = document.getElementById("nazwisko").value
-    console.log('Imie/Nazwisko:' + " " + imie + ' ' + nazwisko)
-    const email = document.getElementById("email").value
-    console.log("Email:" + "" + email)
-    const tel = document.getElementById("tel").value
-    console.log("Telefon:" + "" + tel)
-    const mess = document.getElementById("mess").value
-    console.log("Wiadomość:" + "" + mess)
-    console.log('-----------------')
+function walidacja() { 
+const imie = document.getElementById("name").value
+    console.log(imie)
+const nazwisko = document.getElementById("sname").value
+    console.log(nazwisko)
+const mail = document.getElementById("mail").value
+    console.log(mail)
+const tel = document.getElementById("tel").value
+    console.log(tel)
+const info = document.getElementById("info").value
+    console.log(info)
 }
